9b4b627872c20afd33c33fe33d866dfc1ef9deee
ThinhVu <vutrongthinhk7@gmail.com>
2023-03-22 23:40:06 +0700

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const methods = require("methods");
+
+export default function RouterX(options) {
+  const router = express.Router(options)
+  return new Proxy(router, {
+    get(target, p) {
+      if (methods.includes(p)) {
+        return function(method, metadata, ...args) {
+          if (metadata && typeof metadata == 'object') {
+            target[p](method, ...args)
+            if (router.stack.length) {
+              const layer = router.stack[router.stack.length - 1]
+              layer.route.metadata = metadata
+            }
+          } else {
+            target[p](method, metadata, ...args)
+          }
+        }
+      } else {
+        return Reflect.get(...arguments)
+      }
+    }
+  })
+}
