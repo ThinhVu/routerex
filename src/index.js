b7041684a62f6b82c6bd286884e8acabff15f433
ThinhVu <vutrongthinhk7@gmail.com>
2023-04-16 09:24:02 +0700

    breaking-change: Remove routeMetadataCollectorFactory.js export routerex as default change yarn -> pnpm

--- src/index.js
+++ src/index.js
@@ -1,5 +1,25 @@
-const routerex = require('./routerex')
-const routeMetadataCollectorFactory = require('./routeMetadataCollectorFactory')
+const express = require("express")
+const methods = require("methods")
 
-module.exports = routerex;
-module.exports.routeMetadataCollectorFactory = routeMetadataCollectorFactory;
+module.exports = function(options) {
+  const router = express.Router(options)
+  return new Proxy(router, {
+    get(target, p) {
+      if (methods.includes(p)) {
+        return function(method, metadata, ...args) {
+          if (metadata && typeof metadata == 'object') {
+            target[p](method, ...args)
+            const layer = router.stack[router.stack.length - 1]
+            layer.route.metadata = metadata || {}
+          } else {
+            target[p](method, metadata, ...args)
+            const layer = router.stack[router.stack.length - 1]
+            layer.route.metadata = {}
+          }
+        }
+      } else {
+        return Reflect.get(...arguments)
+      }
+    }
+  })
+}
